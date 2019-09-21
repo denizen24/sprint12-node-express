bed054d272b7ac070a80ce2499baad2fdf1e1d8b
Vladimirov Denis <akakiy813@yandex.ru>
2019-09-21 18:34:31 +0300

    change app.js delete console.log

--- app.js
+++ app.js
@@ -22,7 +22,4 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on port: ${PORT}`);
-});
+app.listen(PORT);
